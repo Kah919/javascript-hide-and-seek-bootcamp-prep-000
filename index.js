2410f0d12ee44dbc83218c428a2a3e54795f5bc9
Kah Yap <kahyap13@gmail.com>
2018-10-30 19:03:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function deepestChild() {
 }
 
 function increaseRankBy(n) {
-  var rank = document.querySelectorAll('.ranked-list').querySelectorAll('li');
+  var rank = document.querySelectorAll('.ranked-list li')
   for(var i = 0; i < rank.length; i++) {
 
   }
