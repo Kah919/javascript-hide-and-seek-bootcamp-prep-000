115cc055093b56de557c1520bea03897f5845a27
Kah Yap <kahyap13@gmail.com>
2018-10-30 19:08:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested').querySelector('.target')
+}
+
+function deepestChild() {
+  var div = document.querySelector('#grand-node').querySelectorAll('div')
+  return div[div.length - 1]
+}
+
+function increaseRankBy(n) {
+  var rank = document.querySelectorAll('.ranked-list li')
+  for(var i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = (parseInt(rank[i].innerHTML) + n)
+  }
+}
