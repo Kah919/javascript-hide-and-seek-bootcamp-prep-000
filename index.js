7b447d24e95c093a329df9760c4ffa9f2a12151e
Kah Yap <kahyap13@gmail.com>
2018-10-30 18:03:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested').querySelector('.target')
 }
+
+function deepestChild() {
+  var div = document.querySelector('#grand-node').querySelector('div')
+  for(var i = 0; i < div.length; i++) {
+    if(i === div.length) {
+      return div
+    }
+  }
+  
+}
